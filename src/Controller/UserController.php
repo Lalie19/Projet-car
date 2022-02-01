2c2de5c75d847a1a662052135491b9b77274812c
Eulalie <petitlalie06@gmail.com>
2022-02-01 11:50:14 +0100

    connexion inscription profil

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -22,11 +22,60 @@ class UserController extends AbstractController
      */
     public function index()
     {
-        $userManager = new UserManager();
-        $users = $userManager->selectAll();
-
-        return $this->twig->render('User/index.html.twig', ['users' => $users]);
+        $this->redirectTo("/user/login");
     }
+    /**
+     * Display user listing
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function login()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $error = false;
+            if (!$_POST['email']) {
+                $this->addFlash("color-danger", "une adresse de courriel est obligatoire");
+                $error = true;
+            }
+            if (!$_POST['password']) {
+                $this->addFlash("color-danger", "vous devez rentrer un mot de passe");
+                $error = true;
+            }
+            if (!$error) {
+                $userManager = new UserManager();
+                $criteria = [
+                    "email" => $_POST["email"],
+                ];
+                $userTab = $userManager->findBy($criteria);
+                $user = $userTab[0];
+                if ($user ?? false) {
+                    $passOk = password_verify($_POST['password'], $user["password"]);
+                    if ($passOk) {
+                        // connexion
+                        $_SESSION['user'] = $user;
+                        unset($_SESSION['user']['password']);
+                        $_SESSION['user']['role'] = json_decode($user['role']);
+                        $this->addFlash("color-success", "vous êtes connecté");
+                        $this->redirectTo("/");
+                    }
+                }
+                $this->addFlash("color-danger", "erreur de mot de passe ou de courriel");
+            }
+        }
+
+        return $this->twig->render('User/login.html.twig');
+    }
+
+    public function logout()
+    {
+        unset($_SESSION["user"]);
+        $this->addFlash("color-success", "vous êtes correctement déconnecté");
+        $this->redirectTo("/");
+    }
+
 
 
     /**
@@ -38,13 +87,15 @@ class UserController extends AbstractController
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
      */
-    public function show(int $id)
+    public function profile()
     {
+        $id = $_SESSION['user']['id'];
         $userManager = new UserManager();
         $user = $userManager->selectOneById($id);
 
-        return $this->twig->render('User/show.html.twig', ['user' => $user]);
+        return $this->twig->render('User/profile.html.twig', ['user' => $user]);
     }
+    
 
 
     /**
@@ -85,11 +136,14 @@ class UserController extends AbstractController
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
      */
-    public function add()
+    public function subscribe()
     {
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            var_dump($_POST);
+            die;
             $userManager = new UserManager();
+            $error = false;
             $user= [
                 "firstname" => $_POST['firstname'],
                 "lastname" => $_POST['lastname'],
@@ -98,11 +152,16 @@ class UserController extends AbstractController
                 "phone" => $_POST['phone'],
                 "password" => $_POST['password'],
             ];
+            if (!$_POST['firstname']) {
+                $this->addFlash("color-danger", "le prénom est obligatoire");
+                $error = true;
+            }
+            
             $id = $userManager->create($user);
             header('Location:/user/show/' . $id);
         }
 
-        return $this->twig->render('User/add.html.twig');
+        return $this->twig->render('User/subscribe.html.twig');
     }
 
 
